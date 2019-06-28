3e548c86386cc934a7fcc1618c3bac27a27d6ae5
Aleksey S <i.am.shep@gmail.com>
2019-06-28 10:40:35 +0300

    Create snmp-item-hex-to-string.js

---
+++ preprocessing/snmp-item-hex-to-string.js
@@ -0,0 +1,45 @@
+// Item preprocessing JavaScript filter
+// function (value) {
+function convert1251toutf8(utftext) {
+    var string = "";
+    var i = 0;
+    var c = c1 = c2 = 0;
+
+    while ( i < utftext.length ) {
+
+        c = utftext.charCodeAt(i);
+
+        if (c < 128) {
+            string += String.fromCharCode(c);
+            i++;
+        }
+        else if((c > 191) && (c < 224)) {
+            c2 = utftext.charCodeAt(i+1);
+            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+            i += 2;
+        }
+        else {
+            c2 = utftext.charCodeAt(i+1);
+            c3 = utftext.charCodeAt(i+2);
+            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+            i += 3;
+        }
+
+    }
+
+    return string;
+}
+
+  var ishex = /^([0-9A-F]\s*)+$/;
+    if (ishex.test(value))
+    {
+      var hex  = value.replace(/[\s\r\n]/g,'');
+    	var str = '';
+    	for (var n = 0; n < hex.length; n += 2) {
+    	  if (hex.substr(n, 2) != "00")
+    		  str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+    	}
+    	value = convert1251toutf8(str);
+    }
+	return value;
+// }
